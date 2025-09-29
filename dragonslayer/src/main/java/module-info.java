4257db1ac08381cac13cf0bcab7e74f8780a30c2
m20re <marvinorellana@unomaha.edu>
2025-09-28 22:23:43 -0500

    Automated jpackage + jlink to create portable applications or MSI installers.

--- dragonslayer/src/main/java/module-info.java
+++ dragonslayer/src/main/java/module-info.java
@@ -5,13 +5,14 @@ module org.unocapstone.dragonslair {
 
     // --- JDBC (Derby discovered via META-INF/services) ---
     requires java.sql;
+    requires java.xml; // for DataSource via JNDI lookup
     requires zt.zip;
 
     requires org.apache.commons.io;
     requires org.apache.poi.ooxml;
     requires org.apache.poi.poi;
 
-    // --- Logging ---
+    // Exposes controllers and related java files to FXMLs reflection system
     opens org.unocapstone.dragonslair to javafx.fxml, javafx.base, javafx.graphics;
     opens org.unocapstone.dragonslair.controllers to javafx.fxml, javafx.base, javafx.graphics;
     opens org.unocapstone.dragonslair.ui to javafx.fxml, javafx.base, javafx.graphics;
