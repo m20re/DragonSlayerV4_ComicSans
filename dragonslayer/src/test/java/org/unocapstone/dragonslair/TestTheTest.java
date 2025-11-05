dfff879ef4da5c206fad6204f4f67e092a52e345
m20re <marvinorellana@unomaha.edu>
2025-11-05 12:25:17 -0600

    Updated testing suite to Junit5 among other POM modifications.

---
+++ dragonslayer/src/test/java/org/unocapstone/dragonslair/TestTheTest.java
@@ -0,0 +1,12 @@
+package org.unocapstone.dragonslair;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestTheTest {
+    @Test
+    void adds() {
+        assertEquals(4, 2+2);
+    }
+}
