676ab4f383b9611dc21511b32e3908b10e26ee47
m20re <marvinorellana@unomaha.edu>
2025-12-04 12:23:10 -0600

    Add file chooser provider for testing in ExportSaveTest

--- dragonslayer/src/test/java/org/unocapstone/dragonslair/ExportSaveTest.java
+++ dragonslayer/src/test/java/org/unocapstone/dragonslair/ExportSaveTest.java
@@ -14,6 +14,7 @@ public class ExportSaveTest extends BaseFxUiTest {
     // Should be changed if a higher amount is warranted
     private static final int    TEST_ITERATIONS = 100;
     private static final String TEST_OUTPUT_DIRECTORY = "test_export_spam";
+    private int currIterations = 0;
 
     // Contains a list of created files, will be constantly deleted as test runs
     private List<File> createdFiles = new ArrayList<File>();
@@ -27,11 +28,23 @@ public class ExportSaveTest extends BaseFxUiTest {
             Files.createDirectories(outputDirectory);
         }
 
+    // Inject the custom FileChooser behavior into the controller
+        controller.setFileChooserProvider((fc, window) -> { // recall that controller is defined within the abstract class
+            String fileName = TEST_OUTPUT_DIRECTORY + "/flagged_export_" + currIterations + ".xlsx";
+            File file = new File(fileName);
+            createdFiles.add(file);
+            return file;
+        });
+
         openReportsTab(robot);
         
-        // Testinig body
+        // Testing body
         for(int i = 0; i < TEST_ITERATIONS; i++) {
-            System.out.println("============== Test " + (i + 1) + " ====================");
+            currIterations += 1;
+            System.out.println("============== Test " + currIterations + " ====================");
+            
+            // click the export button
+            robot.clickOn("#exportNewWeekFlagsCustomerRequestsButton");
             
         }
     }
